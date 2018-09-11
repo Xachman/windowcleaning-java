945bb7c53f793bf623d969cda8980487c5169288
xachman <zironside@hotmail.com>
2018-09-10 22:03:47 -0400

    add fake data for customers

--- src/main/java/com/gti/windowcleaning/controller/CustomersController.java
+++ src/main/java/com/gti/windowcleaning/controller/CustomersController.java
@@ -9,7 +9,9 @@ import com.gti.windowcleaning.data.Customer;
 import com.gti.windowcleaning.model.CustomersModel;
 import java.util.List;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -23,11 +25,10 @@ import javafx.scene.layout.VBox;
 public class CustomersController {
     private CustomersModel customers = new CustomersModel();
     @FXML
-    public VBox scrollPaneVBox;
+    public ScrollPane scrollPane;
 
     @FXML
     public void initialize() {
-        System.out.println(scrollPaneVBox);
         int row = 1;
         GridPane gridPane = new GridPane();
         ColumnConstraints col1 = new ColumnConstraints();
@@ -36,8 +37,10 @@ public class CustomersController {
         col2.setPercentWidth(50);
         ColumnConstraints col3 = new ColumnConstraints();
         col3.setPercentWidth(25);
-        gridPane.getColumnConstraints().addAll(col1, col2, col3);
-
+        
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
         gridPane.add(new Label("Name"),1,0);
         gridPane.add(new Label("Address"),2,0);
         gridPane.add(new Label("Phone"),3,0);
@@ -47,7 +50,7 @@ public class CustomersController {
             gridPane.add(new Label(customer.getPhone()),3,row);
             row++;
         };
-        scrollPaneVBox.getChildren().add(gridPane);
+        scrollPane.setContent(gridPane);
          
         
     } 
--- src/main/java/com/gti/windowcleaning/mocks/StorageMock.java
+++ src/main/java/com/gti/windowcleaning/mocks/StorageMock.java
@@ -7,8 +7,18 @@ package com.gti.windowcleaning.mocks;
 
 import com.gti.windowcleaning.data.Customer;
 import com.gti.windowcleaning.data.StorageI;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -19,13 +29,30 @@ public class StorageMock implements StorageI {
     @Override
     public List<Customer> getCustomers() {
         List<Customer> customers = new ArrayList<>();
-        Customer customer1 = new Customer();
+        JSONParser parser = new JSONParser();
+        try {
+            String fileName = getClass().getResource("/mock/data/customers.json").getFile();
+            FileReader r = new FileReader(fileName);
+            Object obj = parser.parse(r);
+            JSONArray jCustomers = (JSONArray) obj;
+            
+            Iterator<JSONObject> iter = jCustomers.iterator();
 
-        customer1.setName("Tim Jones");
-        customer1.setAddress("123 elm St");
-        customer1.setPhone("555-555-5551");
-
-        customers.add(customer1);
+            while(iter.hasNext()) {
+                JSONObject jobj = iter.next();
+                Customer customer = new Customer();
+                customer.setName(jobj.get("name").toString());
+                customer.setAddress(jobj.get("address").toString());
+                customer.setPhone(jobj.get("phone").toString());
+                customers.add(customer);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(StorageMock.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(StorageMock.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(StorageMock.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return customers;
     }
 
