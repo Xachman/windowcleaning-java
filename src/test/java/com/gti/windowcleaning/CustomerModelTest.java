c7400f0855f015933c70deb93eeb0b7993a65d90
xachman <zironside@hotmail.com>
2018-09-18 10:49:21 -0400

    make validate test more specific

--- src/test/java/com/gti/windowcleaning/CustomerModelTest.java
+++ src/test/java/com/gti/windowcleaning/CustomerModelTest.java
@@ -8,7 +8,7 @@ package com.gti.windowcleaning;
 import com.gti.windowcleaning.data.Customer;
 import com.gti.windowcleaning.model.CustomersModel;
 import com.gti.windowcleaning.model.MustIncludeException;
-import org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -16,12 +16,16 @@ import org.junit.Test;
  * @author xach
  */
 public class CustomerModelTest {
-    @Test(expected =  MustIncludeException.class)
+    @Test(expected = MustIncludeException.class)
     public void validateAllFields() throws MustIncludeException {
         Customer customer = new Customer();
         CustomersModel model = new CustomersModel();
-        
-        model.save(customer);
+        try {
+            model.save(customer);
+        } catch(MustIncludeException e) {
+            Assert.assertEquals("[name, location, area, contact, contactBilling, type]",e.getMessage());
+            throw e;
+        }
         
     }
 }
