36373f0e44d92e5a688af8db9b6728e463068485
xachman <zironside@hotmail.com>
2018-10-02 08:54:01 -0400

    add job test and add annotations to job

--- src/main/java/com/gti/windowcleaning/data/Job.java
+++ src/main/java/com/gti/windowcleaning/data/Job.java
@@ -1,5 +1,6 @@
 package com.gti.windowcleaning.data;
 
+import com.j256.ormlite.field.DatabaseField;
 import java.util.Date;
 
 /*
@@ -13,18 +14,144 @@ import java.util.Date;
  * @author xach
  */
 public class Job {
+    @DatabaseField(generatedId = true)
     private int id;
-    private int customer_id;
+    @DatabaseField(canBeNull = false, foreign = true)
+    private Customer customer;
+    @DatabaseField(canBeNull = false)
     private String serviced_by;
+    @DatabaseField(canBeNull = false)
     private double amount;
+    @DatabaseField(canBeNull = false)
     private Date serviceDate;
+    @DatabaseField()
     private int daysBetween;
+    @DatabaseField()
     private double percent;
+    @DatabaseField(canBeNull = false)
     private boolean printServiceStatement;
+    @DatabaseField(canBeNull = false)
     private boolean printInvoice;
+    @DatabaseField(canBeNull = false)
     private boolean paymentExpected;
+    @DatabaseField()
     private String description;
+    @DatabaseField(canBeNull = false)
     private int difficulty;
+    @DatabaseField()
     private String notes;
+    @DatabaseField(canBeNull = false)
     private boolean autoPrintServiceStatment;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getServiced_by() {
+        return serviced_by;
+    }
+
+    public void setServiced_by(String serviced_by) {
+        this.serviced_by = serviced_by;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getServiceDate() {
+        return serviceDate;
+    }
+
+    public void setServiceDate(Date serviceDate) {
+        this.serviceDate = serviceDate;
+    }
+
+    public int getDaysBetween() {
+        return daysBetween;
+    }
+
+    public void setDaysBetween(int daysBetween) {
+        this.daysBetween = daysBetween;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+
+    public boolean isPrintServiceStatement() {
+        return printServiceStatement;
+    }
+
+    public void setPrintServiceStatement(boolean printServiceStatement) {
+        this.printServiceStatement = printServiceStatement;
+    }
+
+    public boolean isPrintInvoice() {
+        return printInvoice;
+    }
+
+    public void setPrintInvoice(boolean printInvoice) {
+        this.printInvoice = printInvoice;
+    }
+
+    public boolean isPaymentExpected() {
+        return paymentExpected;
+    }
+
+    public void setPaymentExpected(boolean paymentExpected) {
+        this.paymentExpected = paymentExpected;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public boolean isAutoPrintServiceStatment() {
+        return autoPrintServiceStatment;
+    }
+
+    public void setAutoPrintServiceStatment(boolean autoPrintServiceStatment) {
+        this.autoPrintServiceStatment = autoPrintServiceStatment;
+    }
 }
