6085bac0d94291cdd4869da6591461249b946c1d
xachman <zironside@hotmail.com>
2018-10-07 21:11:32 -0400

    create simple framework for testing javafx front

--- src/main/java/com/gti/windowcleaning/controller/AddEditCustomerController.java
+++ src/main/java/com/gti/windowcleaning/controller/AddEditCustomerController.java
@@ -11,8 +11,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
 
 /**
  *
@@ -24,6 +24,12 @@ public class AddEditCustomerController implements Initializable {
 
     @FXML
     private TextField name;
+    @FXML
+    private CheckBox active;
+    @FXML
+    private TextField incactiveSince;
+    
+
     
     public void setId(int id) {
         this.id = id;
--- src/test/java/com/gti/windowcleaning/controller/AddEditCustomerControllerTest.java
+++ src/test/java/com/gti/windowcleaning/controller/AddEditCustomerControllerTest.java
@@ -5,63 +5,40 @@
  */
 package com.gti.windowcleaning.controller;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.testfx.framework.junit.ApplicationTest;
-import org.testfx.util.WaitForAsyncUtils;
 
 /**
  *
  * @author xach
  */
-public class AddEditCustomerControllerTest extends ApplicationTest {
-    private Parent root; 
-    
-    private CheckBox active; 
-    private Button update;
-    private TextField name;
-    
-    @Override
-    public void start(Stage stage) throws IOException {
-            root = FXMLLoader.load(AddEditCustomerController.class.getResource("/view/customer/add_edit.fxml"));
-            stage.setScene(new Scene(root));
-            stage.show();
-            stage.toFront();
-    }
-   
-        /* Just a shortcut to retrieve widgets in the GUI. */
-    public <T extends Node> T find(final String query) {
-        /** TestFX provides many operations to retrieve elements from the loaded GUI. */
-        return lookup(query).query();
-    }
+public class AddEditCustomerControllerTest extends ControllerTest {
+
    
     @Before
     public void setUp() {
-        /* Just retrieving the tested widgets from the GUI. */
-        active = find("#active");
-        update = find("#update");
-        name = find("#name");
+        setConroller(new AddEditCustomerController());
     }
+
     @Test
     public void addNewCustomer() {
-        clickOn(active);
-        
-        WaitForAsyncUtils.waitForFxEvents();
-        clickOn(update);
-        WaitForAsyncUtils.waitForFxEvents();
-        Assert.assertEquals("test", name.getText());
+            try {
+
+            TextField name = new TextField();
+            name.setText("ted");
+            setFXMLField("name", name);
+            
+            Assert.assertEquals(0, 0);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+    
     }
+
+
+
+
     
 }
