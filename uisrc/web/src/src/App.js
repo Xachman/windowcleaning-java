f8d493723d6f95357b030166ad0b221391db81a7
xachman <zironside@hotmail.com>
2018-10-19 21:30:13 -0400

    add CustomerCreate and CustomerEdit components

--- uisrc/web/src/src/App.js
+++ uisrc/web/src/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import simpleRestProvider from 'ra-data-simple-rest';
-import { CustomersList, CustomerCreate } from './resource/customers'
+import { CustomersList, CustomerCreate, CustomerEdit } from './resource/customers'
 const dataProvider = simpleRestProvider('http://localhost:8080');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="customers" list={CustomersList} create={CustomerCreate} />
+    <Resource name="customers" list={CustomersList} edit={CustomerEdit} create={CustomerCreate}  />
   </Admin>
 )
 
--- uisrc/web/src/src/resource/customers.js
+++ uisrc/web/src/src/resource/customers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List, Datagrid, TextField, Create, TextInput, LongTextInput, SimpleForm } from 'react-admin';
+import { List, Edit, DisabledInput, EditButton, Datagrid, DateInput, NumberInput, SelectInput, TextField, Create, TextInput, LongTextInput, TabbedForm, FormTab, BooleanInput, TabbedShowLayout, Tab } from 'react-admin';
 
 export const CustomersList = (props) => (
     <List {...props}>
@@ -7,16 +7,133 @@ export const CustomersList = (props) => (
             <TextField source="id" />
             <TextField source="name" />
             <TextField source="location" />
+            <EditButton />
         </Datagrid>
     </List>
 );
 
 export const CustomerCreate = (props) => (
     <Create {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="location" />
-            <LongTextInput source="body" />
-        </SimpleForm>
+        <TabbedForm>
+            <FormTab label="General">
+                <TextInput source="name" />
+                <TextInput source="location" />
+                <TextInput source="area" />
+                <SelectInput source="type" choices={[
+                    { id: 'C', name: 'C' },
+                    { id: 'N', name: 'N' },
+                    { id: 'R', name: 'R' },
+                ]} />
+
+                <SelectInput source="company" choices={[
+                    { id: 'S', name: 'S' },
+                    { id: 'I', name: 'I' },
+                ]} />
+                <LongTextInput source="notes" />
+            </FormTab>
+            <FormTab label="Contacts">
+                <h2>Location Information</h2>
+                <TextInput source="contact" />
+                <TextInput source="addressLine1" />
+                <TextInput source="addressLine2" />
+                <TextInput source="city" />
+                <TextInput source="state" />
+                <TextInput source="zipcode" />
+                <TextInput source="phone" />
+                <TextInput source="fax" />
+                <h2>Billing Information</h2>
+                <TextInput source="contactB" />
+                <TextInput source="addressLine1B" />
+                <TextInput source="addressLine2B" />
+                <TextInput source="cityB" />
+                <TextInput source="stateB" />
+                <TextInput source="zipcodeB" />
+                <TextInput source="phoneB" />
+                <TextInput source="faxB" />
+            </FormTab>
+            <FormTab label="Accounting">
+                <TextInput source="obtainedBy"/>
+                <TextInput source="royaltyTo"/>
+                <NumberInput source="monthlyBill" placeholder="0.0" />
+                <NumberInput source="billCharge" placeholder="0.0"/>
+                <NumberInput source="deposit" placeholder="0.0"/>
+                <NumberInput source="salesTax" placeholder="0.0"/>
+            </FormTab>
+            <FormTab label="Settings">
+                <DateInput source="customerSince" />
+                <BooleanInput source="active" />
+                <DateInput source="inactiveSince" />
+                <BooleanInput source="fourWeekInvoice" />
+                <BooleanInput source="calendarMonthInvoice" />
+                <BooleanInput source="quarterlyInvoice" />
+                <BooleanInput source="AWWPUR" />
+                <BooleanInput source="AWWLine" />
+            </FormTab>
+        </TabbedForm>
     </Create>
+);
+const CustomerTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.name}"` : ''}</span>;
+};
+export const CustomerEdit = (props) => (
+    <Edit title={<CustomerTitle/>} {...props}>
+        <TabbedForm>
+
+            <DisabledInput source="id" />
+            <FormTab label="General">
+                <TextInput source="name" />
+                <TextInput source="location" />
+                <TextInput source="area" />
+                <SelectInput source="type" choices={[
+                    { id: 'C', name: 'C' },
+                    { id: 'N', name: 'N' },
+                    { id: 'R', name: 'R' },
+                ]} />
+
+                <SelectInput source="company" choices={[
+                    { id: 'S', name: 'S' },
+                    { id: 'I', name: 'I' },
+                ]} />
+                <LongTextInput source="notes" />
+            </FormTab>
+            <FormTab label="Contacts">
+                <h2>Location Information</h2>
+                <TextInput source="contact" />
+                <TextInput source="addressLine1" />
+                <TextInput source="addressLine2" />
+                <TextInput source="city" />
+                <TextInput source="state" />
+                <TextInput source="zipcode" />
+                <TextInput source="phone" />
+                <TextInput source="fax" />
+                <h2>Billing Information</h2>
+                <TextInput source="contactB" />
+                <TextInput source="addressLine1B" />
+                <TextInput source="addressLine2B" />
+                <TextInput source="cityB" />
+                <TextInput source="stateB" />
+                <TextInput source="zipcodeB" />
+                <TextInput source="phoneB" />
+                <TextInput source="faxB" />
+            </FormTab>
+            <FormTab label="Accounting">
+                <TextInput source="obtainedBy"/>
+                <TextInput source="royaltyTo"/>
+                <NumberInput source="monthlyBill" placeholder="0.0" />
+                <NumberInput source="billCharge" placeholder="0.0"/>
+                <NumberInput source="deposit" placeholder="0.0"/>
+                <NumberInput source="salesTax" placeholder="0.0"/>
+            </FormTab>
+            <FormTab label="Settings">
+                <DateInput source="customerSince" />
+                <BooleanInput source="active" />
+                <DateInput source="inactiveSince" />
+                <BooleanInput source="fourWeekInvoice" />
+                <BooleanInput source="calendarMonthInvoice" />
+                <BooleanInput source="quarterlyInvoice" />
+                <BooleanInput source="AWWPUR" />
+                <BooleanInput source="AWWLine" />
+            </FormTab>
+        </TabbedForm>
+    </Edit>
 );
