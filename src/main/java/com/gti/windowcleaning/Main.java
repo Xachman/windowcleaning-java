0d20aee4d59cd40e17c8d1df5a8885da019e377e
xachman <zironside@hotmail.com>
2018-10-21 19:23:18 -0400

    remove javafx

--- src/main/java/com/gti/windowcleaning/Main.java
+++ src/main/java/com/gti/windowcleaning/Main.java
@@ -8,39 +8,43 @@ package com.gti.windowcleaning;
 import com.gti.windowcleaning.data.Customer;
 import com.gti.windowcleaning.data.Job;
 import com.gti.windowcleaning.data.SQLiteStorage;
+import com.gti.windowcleaning.model.CustomersModel;
+import com.gti.windowcleaning.web.controller.customers.CustomerController;
+import com.gti.windowcleaning.web.controller.customers.CustomersController;
+import com.gti.windowcleaning.web.controller.customers.DeleteCustomerController;
+import com.gti.windowcleaning.web.controller.customers.EditCreateCustomerController;
 import java.io.File;
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.MenuBar;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import spark.Request;
+import static spark.Spark.*;
 /**
  *
  * @author xach
  */
-public class Main extends Application {
-    Scene scene;
-    @FXML
-    private AnchorPane rootPane; 
-    @FXML
-    private VBox rootVBox;
+public class Main {
+    public static void main(String[] args) {
+        startWeb();
+    } 
 
-    @FXML
-    private MenuBar menuBar; 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
-        scene = new Scene(root, 1000, 720);
-        scene.getStylesheets().add(getClass().getResource("/css/main.css").toString());
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        System.out.println(scene);
-        System.out.println(scene.getStylesheets());
+    private static String requestInfoToString(Request request) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(request.requestMethod());
+        sb.append(" " + request.url());
+        sb.append(" " + request.body());
+        if(request.queryString() != null) {
+            try {
+                sb.append(" " + URLDecoder.decode(request.queryString(), "UTF-8"));
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void startWeb() {
         String storagePath = System.getProperty("user.home")+File.separator+".windowcleaning";
         File storageDir = new File(storagePath); 
         if(!storageDir.exists()){
@@ -51,7 +55,26 @@ public class Main extends Application {
         SQLiteStorage storage = new SQLiteStorage(storagePath+File.separator+"data.db");
         storage.create(Customer.class);
         storage.create(Job.class);
+        staticFiles.location("/webui");
+        port(8080);
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+            response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+            response.header("Access-Control-Expose-Headers", "Content-Range");
+            System.out.println(requestInfoToString(request));
+        });
+        get("/ui",(req, res) -> {
+            res.redirect("/index.html");
+            return null;
+        });
+        get("/customers",new CustomersController(new CustomersModel(storage)));
+        options("/customers",new CustomersController(new CustomersModel(storage)));
+        
+        get("/customers/:id",new CustomerController(new CustomersModel(storage)));
+        options("/customers/:id",new CustomerController(new CustomersModel(storage)));
+        delete("/customers/:id",new DeleteCustomerController(new CustomersModel(storage)));
+        post("/customers",new EditCreateCustomerController(new CustomersModel(storage)));
+        put("/customers/:id",new EditCreateCustomerController(new CustomersModel(storage)));
     }
-
- 
 }
---
+++ src/main/resources/webui/precache-manifest.ec00560446a5323ab1a2a7ee2cf6a502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b568c5698033121fba8d",
+    "url": "/static/js/main.b568c569.chunk.js"
+  },
+  {
+    "revision": "578664d26cfcb81a8278",
+    "url": "/static/js/1.578664d2.chunk.js"
+  },
+  {
+    "revision": "b568c5698033121fba8d",
+    "url": "/static/css/main.4d33e90b.chunk.css"
+  },
+  {
+    "revision": "f4543faef227f7219353ba3a9fa6cc13",
+    "url": "/index.html"
+  }
+];
