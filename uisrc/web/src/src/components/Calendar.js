dbf00f4492aabbb038b6e1e07613a61a76097a4a
xachman <zironside@hotmail.com>
2018-10-30 12:58:27 -0400

    add fullcalendar

--- uisrc/web/src/src/components/Calendar.js
+++ uisrc/web/src/src/components/Calendar.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import { Config } from '../Config';
 import { GET_LIST } from 'react-admin';
+import $ from 'jquery'; 
+import 'moment/min/moment.min.js';
+
+import 'fullcalendar/dist/fullcalendar.css';
+import 'fullcalendar/dist/fullcalendar.js';
 
 export class Calendar extends React.Component {
     dataProvider = Config.getDataProvider();
+    cal;
     constructor(props) {
         super(props)
-        this.dataProvider(GET_LIST, 'jobs', {}).then((data) => {
-            console.log(data)
-        })
+    }
+    componentDidMount() {
+        this.cal = $(this.refs.calendar).fullCalendar()
+        let date = new Date();
+        this.getEvents(new Date(date.getFullYear(), date.getMonth(), 1),
+            new Date(date.getFullYear(), date.getMonth()+1, 0) 
+        )
     }
     render() {
-      return <h1>Hello, {this.props.name}</h1>;
+      return <div ref="calendar"></div>;
     }
+
+    getEvents(startDate, endDate) {
+
+        this.dataProvider(GET_LIST, 'jobs', {
+            between: {
+                serviceDate: [startDate.getTime(),endDate.getTime()]
+            }
+        }).then((data) => {
+            console.log(data)
+            console.log(this.cal)
+        })
+    }
+
+    arrangeEvents(data) {
+        result = [];
+        for(var i = 0; i < data.length; i++ ){
+            item = data[i];
+            result.push({
+                
+            })
+        }
+    }
+
+
+
 }
