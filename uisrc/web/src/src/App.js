b4ded23d10ed508cc8dc068981baae1df569109e
xach <zironside@hotmail.com>
2018-11-04 07:59:03 -0500

    add create job to ui

--- uisrc/web/src/src/App.js
+++ uisrc/web/src/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import { CustomersList, CustomerCreate, CustomerEdit, CustomerShow } from './resource/customers'
-import { JobsList } from './resource/jobs'
+import { JobsList, CreateJob } from './resource/jobs'
 import Routes from './Routes';
 import { Config } from './Config';
 import AppLayout from './AppLayout';
@@ -10,7 +10,8 @@ const dataProvider = new Config.getDataProvider()
 const App = () => (
   <Admin appLayout={AppLayout} customRoutes={Routes} dataProvider={dataProvider}>
     <Resource name="customers" list={CustomersList} edit={CustomerEdit} create={CustomerCreate} show={CustomerShow} />
-    <Resource name="jobs" list={JobsList}  />
+    
+    <Resource name="jobs" list={JobsList} create={CreateJob}  />
   </Admin>
 )
 
--- uisrc/web/src/src/resource/jobs.js
+++ uisrc/web/src/src/resource/jobs.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { List, Edit, DateField, ReferenceField, EditButton, Datagrid, DateInput, NumberInput, SelectInput, TextField, Create, TextInput, LongTextInput, TabbedForm, FormTab, BooleanInput, TabbedShowLayout, Tab } from 'react-admin';
+import { List, Edit, DateField, ReferenceField, EditButton, Datagrid, DateInput, NumberInput, SelectInput, 
+    TextField, Create, TextInput, LongTextInput, TabbedForm, FormTab, BooleanInput, 
+    TabbedShowLayout, Tab, SimpleForm, ReferenceInput } from 'react-admin';
 import Grid from '@material-ui/core/Grid';
 
 export const JobsList = (props) => (
@@ -13,4 +15,26 @@ export const JobsList = (props) => (
             <EditButton />
         </Datagrid>
     </List>
+)
+
+export const CreateJob = (props) => (
+    <Create title="Create Job" {...props}>
+        <SimpleForm>
+            <ReferenceInput label="Customer" source="customer.id" reference="customers">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="servicedBy" />
+            <DateInput source="serviceDate" />
+            <NumberInput source="amount" />
+            <NumberInput source="daysBetween" />
+            <NumberInput source="percent" />
+            <BooleanInput source="printServiceStatement" />
+            <BooleanInput source="printInvoice" />
+            <BooleanInput source="paymentExpected" />
+            <BooleanInput source="autoPrintServiceStatement" />
+            <NumberInput source="difficulty" />
+            <LongTextInput source="description" />
+            <LongTextInput source="notes" />
+        </SimpleForm>
+    </Create>
 )
