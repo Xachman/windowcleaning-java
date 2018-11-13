c0300caaf587538651531d51a09fcb7777f9c1b4
xachman <zironside@hotmail.com>
2018-11-13 07:19:05 -0500

    add getters and setters to invoices

--- src/main/java/com/gti/windowcleaning/data/Invoice.java
+++ src/main/java/com/gti/windowcleaning/data/Invoice.java
@@ -26,4 +26,76 @@ public class Invoice extends BaseData {
     private Date writeOffDate;
     @DatabaseField
     private Double writeOffAmount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Date getDateInvoiced() {
+        return dateInvoiced;
+    }
+
+    public void setDateInvoiced(Date dateInvoiced) {
+        this.dateInvoiced = dateInvoiced;
+    }
+
+    public Date getDateRecived() {
+        return dateRecived;
+    }
+
+    public void setDateRecived(Date dateRecived) {
+        this.dateRecived = dateRecived;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmountRecived() {
+        return amountRecived;
+    }
+
+    public void setAmountRecived(Double amountRecived) {
+        this.amountRecived = amountRecived;
+    }
+
+    public Double getStateTax() {
+        return stateTax;
+    }
+
+    public void setStateTax(Double stateTax) {
+        this.stateTax = stateTax;
+    }
+
+    public Date getWriteOffDate() {
+        return writeOffDate;
+    }
+
+    public void setWriteOffDate(Date writeOffDate) {
+        this.writeOffDate = writeOffDate;
+    }
+
+    public Double getWriteOffAmount() {
+        return writeOffAmount;
+    }
+
+    public void setWriteOffAmount(Double writeOffAmount) {
+        this.writeOffAmount = writeOffAmount;
+    }
 }
