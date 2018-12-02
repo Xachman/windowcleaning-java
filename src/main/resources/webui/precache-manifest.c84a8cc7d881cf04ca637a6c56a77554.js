5a2549310dbed6a79f1b9777480054958bb391d8
xach <zironside@hotmail.com>
2018-12-02 17:38:52 +0000

    update ui assets

---
+++ src/main/resources/webui/precache-manifest.c84a8cc7d881cf04ca637a6c56a77554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d6b219b0b3c5fe98361e",
+    "url": "/static/js/main.d6b219b0.chunk.js"
+  },
+  {
+    "revision": "0825da43860b2f02ee85",
+    "url": "/static/js/1.0825da43.chunk.js"
+  },
+  {
+    "revision": "d6b219b0b3c5fe98361e",
+    "url": "/static/css/main.66d1b08a.chunk.css"
+  },
+  {
+    "revision": "0825da43860b2f02ee85",
+    "url": "/static/css/1.2886ceb3.chunk.css"
+  },
+  {
+    "revision": "b383b4ea7c211abb599c83d2cd278375",
+    "url": "/index.html"
+  }
+];
--- src/main/resources/webui/service-worker.js
+++ src/main/resources/webui/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ec00560446a5323ab1a2a7ee2cf6a502.js"
+  "/precache-manifest.c84a8cc7d881cf04ca637a6c56a77554.js"
 );
 
 workbox.clientsClaim();
